71c073c47d145062560afdac7ce70c2cdd318a22
Jacob Simpson <Jacob.Simpson@ssi-schaefer.us>
2019-05-21 15:30:29 -0400

    Navigation Drawer Implementation

--- app/src/main/java/com/example/timekeepers/MainActivity.kt
+++ app/src/main/java/com/example/timekeepers/MainActivity.kt
@@ -1,12 +1,90 @@
 package com.example.timekeepers
 
-import android.support.v7.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_dashboard -> {
+                Toast.makeText(this, "Clicked " + item.title,
+                        Toast.LENGTH_SHORT).show()
+                drawer.closeDrawer(GravityCompat.START)
+            }
+            R.id.nav_management -> {
+                Toast.makeText(this, "Clicked " + item.title,
+                        Toast.LENGTH_SHORT).show()
+                drawer.closeDrawer(GravityCompat.START)
+            }
+            R.id.nav_accounting -> {
+                Toast.makeText(this, "Clicked " + item.title,
+                        Toast.LENGTH_SHORT).show()
+                drawer.closeDrawer(GravityCompat.START)
+            }
+            R.id.nav_budgeting -> {
+                Toast.makeText(this, "Clicked " + item.title,
+                        Toast.LENGTH_SHORT).show()
+                drawer.closeDrawer(GravityCompat.START)
+            }
+            R.id.nav_calendar -> {
+                Toast.makeText(this, "Clicked " + item.title,
+                        Toast.LENGTH_SHORT).show()
+                drawer.closeDrawer(GravityCompat.START)
+            }
+            R.id.nav_settings -> {
+                Toast.makeText(this, "Clicked " + item.title,
+                        Toast.LENGTH_SHORT).show()
+                drawer.closeDrawer(GravityCompat.START)
+            }
+        }
+        return true
     }
 }
---
+++ app/src/main/java/com/example/timekeepers/NavigationDrawer.kt
@@ -0,0 +1,7 @@
+package com.example.timekeepers
+
+import android.support.v7.app.AppCompatActivity
+
+class NavigationDrawer : AppCompatActivity() {
+    
+}
