081665d3adee8ccc83145c79f072602520c4ed78
Jacob Simpson <jsimpson@email.sc.edu>
2019-05-31 21:39:03 -0400

    fff

--- app/src/main/java/com/example/timekeepers/LoginActivity.java
+++ app/src/main/java/com/example/timekeepers/LoginActivity.java
@@ -77,7 +77,6 @@ public class LoginActivity extends AppCompatActivity
         userAuth = FirebaseAuth.getInstance();
         GoogleSignInOptions gso =
                 new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                        .requestIdToken(getString(R.string.default_web_client_id))
                         .requestEmail()
                         .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
