8ecf976c4c85d1c7c5a558ed2d28871a973d4b86
Jake Simpson <simpson.jacob.barry@gmail.com>
2019-06-05 19:56:04 -0400

    Create Job Form is completed (minus some minor changes)

--- app/src/main/java/com/example/timekeepers/JobManagement/AddJob.java
+++ app/src/main/java/com/example/timekeepers/JobManagement/AddJob.java
@@ -1,16 +1,12 @@
 package com.example.timekeepers.JobManagement;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatSpinner;
-import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,18 +15,24 @@ import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
-import com.example.timekeepers.CurrencyAdapter;
+import com.example.timekeepers.CurrencyTextListener;
 import com.example.timekeepers.MainActivity;
 import com.example.timekeepers.R;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
-import static android.content.ContentValues.TAG;
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -48,6 +50,8 @@ public class AddJob extends Fragment
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private final String addJobTitle = "Add Job";
+
+    private MainActivity mainActivity;
 
     // View Declarations
     private View fragmentView;
@@ -115,6 +119,7 @@ public class AddJob extends Fragment
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
         }
+        mainActivity = (MainActivity) getActivity();
     }
 
     @Override
@@ -124,9 +129,8 @@ public class AddJob extends Fragment
         fragmentView = inflater.inflate(R.layout.fragment_add_job, container, false);
 
         // Set Toolbar Title and Lock Navigation Drawer
-        ((MainActivity) Objects.requireNonNull(getActivity()))
-                .toolbar.setTitle(addJobTitle + " (" + mParam1 + ")");
-        ((MainActivity) getActivity()).lockNavigationDrawer(true);
+        mainActivity.toolbar.setTitle(addJobTitle + " (" + mParam1 + ")");
+        mainActivity.lockNavigationDrawer(true);
 
         // Initialize form declarations
         initJobDetailDeclarations();
@@ -148,8 +152,8 @@ public class AddJob extends Fragment
         jobTitle = fragmentView.findViewById(R.id.job_title);
         completedCheckbox = fragmentView.findViewById(R.id.completed_checkbox);
         payRate = fragmentView.findViewById(R.id.pay_rate);
-        CurrencyAdapter currencyAdapter = new CurrencyAdapter(getContext(), payRate);
-        payRate.addTextChangedListener(currencyAdapter);
+        CurrencyTextListener currencyTextListener = new CurrencyTextListener(payRate);
+        payRate.addTextChangedListener(currencyTextListener);
 
         if (mParam1.equals(getString(R.string.salary))) {
             setTextInputLayoutHint("Annual Salary");
@@ -232,10 +236,144 @@ public class AddJob extends Fragment
 
     public void onClick(View view) {
         if (view == saveButton) {
-            Objects.requireNonNull(getActivity()).onBackPressed();
+            db = initializeFirestoreInstance();
+            updateUserJobData();
+            saveJob();
         }
         if (view == cancelButton) {
             Objects.requireNonNull(getActivity()).onBackPressed();
         }
+    }
+
+    private FirebaseFirestore db;
+    private FirebaseFirestore initializeFirestoreInstance() {
+        return FirebaseFirestore.getInstance();
+    }
+    private void updateUserJobData() {
+        final DocumentReference jobData = db.collection("Jobs")
+                .document(mainActivity.getUsersEmail());
+
+        db.collection("Jobs")
+                .document(mainActivity.getUsersEmail())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            jobData.update("Total_Jobs", FieldValue.increment(1));
+                            if (!completedCheckbox.isChecked()) {
+                                jobData.update("Active_Jobs", FieldValue.increment(1));
+                            }
+                            Log.d(TAG, "onSuccess: updateUserJobData");
+                        } else {
+                            setDefaultUserJobData();
+                            updateUserJobData();
+                        }
+                    }
+                });
+    }
+    private void setDefaultUserJobData() {
+        Map<String, Object> defaultData = new HashMap<>();
+        defaultData.put("Total_Jobs", 0);
+        defaultData.put("Active_Jobs", 0);
+        defaultData.put("Gross_Pay", 0);
+
+        db.collection("Jobs")
+                .document(mainActivity.getUsersEmail())
+                .set(defaultData);
+
+        Log.d(TAG, "setDefaultUserJobData: ");
+    }
+
+    private void saveJob() {
+        // Get Job Details from Text Views
+        String job_title = Objects.requireNonNull(jobTitle.getText()).toString().trim();
+        Boolean completed_checkbox = completedCheckbox.isChecked();
+        DecimalFormat df = new DecimalFormat(".00");
+        String pay_rate = Objects.requireNonNull(payRate.getText()).toString();
+        double pay = Double.valueOf(pay_rate.replaceAll("[$,',']", ""));
+        pay = Double.valueOf(df.format(pay));
+        // TODO: Add feature for pay period
+
+        // Get Address Info from Text Views
+        String street_1 = addressLine1.getText().toString().trim();
+        String street_2 = addressLine2.getText().toString().trim();
+        String city_name = city.getText().toString().trim();
+        String state_name = state.getText().toString().trim();
+        String zip_code = zipcode.getText().toString().trim();
+
+        // Get Job Contact Info from Text Views
+        String phone_number = phoneNumber.getText().toString().trim();
+        String job_email = jobEmail.getText().toString().trim();
+        String web = website.getText().toString().trim();
+
+        // Get Tax Info from Text Views
+        String federal_income = federalIncome.getText().toString().trim();
+        String state_income = stateIncome.getText().toString().trim();
+        String social_security = socialSecurity.getText().toString().trim();
+        String medicare = medicareInput.getText().toString().trim();
+        String individual_retirement = individualRetirement.getText().toString().trim();
+        String other_withholdings = otherWithholdings.getText().toString().trim();
+
+        // Hash map the address
+        Map<String, Object> address = new HashMap<>();
+        address.put("Street_1", street_1);
+        address.put("Street_2", street_2);
+        address.put("City", city_name);
+        address.put("State", state_name);
+        address.put("Zip_Code", zip_code);
+
+        // Hash map that crap
+        Map<String, Object> newJob = new HashMap<>();
+        newJob.put("Job_Title", job_title);
+        newJob.put("Completed", completed_checkbox);
+        newJob.put("Pay_Rate", pay);
+        newJob.put("Gross_Pay", 0);
+        newJob.put("Phone", phone_number);
+        newJob.put("Email", job_email);
+        newJob.put("Website", web);
+        newJob.put("Hours_Worked", 0);
+        newJob.put("Address", address);
+
+        // Tax and Withholding Information
+        if (!federal_income.isEmpty()) {
+            newJob.put("Federal_Income_Tax", Double.valueOf(federal_income));
+        } else {
+            newJob.put("Federal_Income_Tax", 0);
+        }
+        if (!state_income.isEmpty()) {
+            newJob.put("State_Income_Tax", Double.valueOf(state_income));
+        } else {
+            newJob.put("State_Income_Tax", 0);
+        }
+        if (!social_security.isEmpty()) {
+            newJob.put("OASDI", Double.valueOf(social_security));
+        } else {
+            newJob.put("OASDI", 0);
+        }
+        if (!medicare.isEmpty()) {
+            newJob.put("Medicare", Double.valueOf(medicare));
+        } else {
+            newJob.put("Medicare", 0);
+        }
+        if (!individual_retirement.isEmpty()) {
+            newJob.put("Retirement_Contribution", Double.valueOf(individual_retirement));
+        } else {
+            newJob.put("Retirement_Contribution", 0);
+        }
+        if (!other_withholdings.isEmpty()) {
+            newJob.put("Other_Withholdings", Double.valueOf(other_withholdings));
+        } else {
+            newJob.put("Other_Withholdings", 0);
+        }
+
+        Log.d(TAG, "saveJob: ");
+        db.collection("Jobs").document(mainActivity.getUsersEmail())
+                .collection("Users_Jobs")
+                .document()
+                .set(newJob);
+
+        // Go back to Job Management page
+        Objects.requireNonNull(getActivity()).onBackPressed();
     }
 }
